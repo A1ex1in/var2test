53310306a43da5ce8d21f530a317a440042c2a86
Alexander <all.ssd@mail.ru>
2024-11-12 14:51:08 +0600

    update user interface

--- app/src/main/java/com/example/var2test/UserInterface.kt
+++ app/src/main/java/com/example/var2test/UserInterface.kt
@@ -1,12 +1,19 @@
 package com.example.var2test
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class UserInterface : AppCompatActivity() {
+
+    private lateinit var settingsButton: ImageButton
+    private lateinit var redactProffileButton: ImageButton
+    private lateinit var selectKeyButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,20 @@ class UserInterface : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        settingsButton = findViewById(R.id.settingsButton)
+        redactProffileButton = findViewById(R.id.redactProffileButton)
+        selectKeyButton = findViewById(R.id.selectKeyButton)
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this, "тык1", Toast.LENGTH_SHORT).show()
+        }
+        redactProffileButton.setOnClickListener {
+            Toast.makeText(this, "тык3", Toast.LENGTH_SHORT).show()
+        }
+        selectKeyButton.setOnClickListener {
+            Toast.makeText(this, "тык2", Toast.LENGTH_SHORT).show()
         }
     }
 }
